0a03743c587309368f4f46611e2be7618fec358c
Xingtian Ma <ma.xingtian04@gmail.com>
2024-09-24 19:45:30 -0400

    img debug

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -128,6 +128,7 @@ export default function Lab1() {
           id="wd-starship"
           width="400px"
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          alt="starship"
         />
         <br />
         Loading a local image:
