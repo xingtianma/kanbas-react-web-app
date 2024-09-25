656c92dbdd15e0f2cfb5650a3dcec9fe379ad15a
Xingtian Ma <ma.xingtian04@gmail.com>
2024-09-24 22:34:02 -0400

    changed button overlay

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -2,14 +2,14 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
+      <button>Unpublish</button> <button>Publish</button><br /><br />
       
-      <button>Import Existing Content</button>
-      <button>Import from Commons</button>
-      <button>Choose Home Page</button>
-      <button>View Course Stream</button>
-      <button>New Announcement</button>
-      <button>New Analytics</button>
-      <button>View Course Notifications</button>
+      <button>Import Existing Content</button><br />
+      <button>Import from Commons</button><br />
+      <button>Choose Home Page</button><br />
+      <button>View Course Stream</button><br />
+      <button>New Announcement</button><br />
+      <button>New Analytics</button><br />
+      <button>View Course Notifications</button><br />
     </div>
 );}
